3a73988837ceadc2e87ce126096208cc6ce4664d
FIvanO <FIvanO1998@gmail.com>
2018-02-08 20:44:21 +0200

    Working version
    Working version of MT, but may be not final.

--- MT_Emulator.cpp
+++ MT_Emulator.cpp
@@ -1,5 +1,8 @@
 //FIvanO
 #include <bits/stdc++.h>
+#include <conio.h>
+#include <stdio.h>
+#include <windows.h>
 
 using namespace std;
 
@@ -34,6 +37,52 @@ int main()
         f >> symb[m][k[m]];
         f >> Move[m][k[m]++];
         //cout << i+1 << ") q" << m << q[m][k[m] - 1] << "  ->  q" << n_st[m][k[m] - 1] << symb[m][k[m] - 1] << Move[m][k[m] - 1] << endl;
+    }
+
+    cout << "Enter the machine's string: ";
+    cin >> mt;
+    mt = 'e' + mt + 'e';
+
+    int state = 0;
+    int pos = 1;
+    char rd = mt[pos];
+    int l = mt.length();
+
+    while (1) {
+        cin.get();
+        int i = 0;
+        cout << mt << "; state: q" << state << endl;
+        for (int i = 0; i < pos; ++i) cout << " ";
+        cout << "^" << endl;
+        rd = mt[pos];
+        int ns = -1; // number of symbol
+        int m = k[state];
+        for (int i = 0; i < m; ++i) { // searching the number of symbol
+            if (rd == q[state][i]) {
+                ns = i;
+                break;
+            }
+        }
+        if (symb[state][ns] != rd) { // replacing the symbols
+            mt[pos] = symb[state][ns];
+            if (pos == l - 1) {
+                mt = mt + 'e';
+                l++;
+            }
+            if (pos == 0) {
+                mt = 'e' + mt;
+                pos++;
+                l++;
+            }
+        }
+        if (Move[state][ns] == 'R') pos++; else {
+            if (Move[state][ns] == 'L') pos--; else {
+                while (mt[1] == 'e') mt.erase(0, 1);
+                cout << "Result: " << mt << endl;
+                return 0;
+            }
+        }
+        state = n_st[state][ns];
     }
 
     return 0;
